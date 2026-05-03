c9f8f4dc646ea541618d8941906055146bcfc0d7
SyedAliArman <armanali16234@gmail.com>
2026-05-04 03:29:38 +0500

    updated app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ jwt = JWTManager(app)
 # =============================
 app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER")
 app.config['MAIL_PORT'] = int(os.getenv("MAIL_PORT"))
-app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', 'True') == 'True'
+app.config['MAIL_USE_SSL'] = 'True'
 app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
 app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv("MAIL_DEFAULT_SENDER")
